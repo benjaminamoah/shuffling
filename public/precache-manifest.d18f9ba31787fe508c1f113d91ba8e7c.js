dbd5260f40c5597641c88f4dac4ab88a58c6ccff
Benjamin Amoah <amoah.benjamin@gmail.com>
2019-08-21 12:00:47 -0400

    update

--- public/precache-manifest.d18f9ba31787fe508c1f113d91ba8e7c.js
+++ public/precache-manifest.d18f9ba31787fe508c1f113d91ba8e7c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a5a1c94994cc36a1eeaeb9b5139240e2",
+    "revision": "0e7750a0d1b90301fc99cc7b20b8864d",
     "url": "/index.html"
   },
   {
-    "revision": "a435c98ee94f8df9b4e0",
-    "url": "/static/css/main.6a4b5090.chunk.css"
+    "revision": "44687b0d256ffa86fc19",
+    "url": "/static/css/main.65ad4a58.chunk.css"
   },
   {
     "revision": "c79f2ab33b1afe0fe496",
     "url": "/static/js/2.6d328d6d.chunk.js"
   },
   {
-    "revision": "a435c98ee94f8df9b4e0",
-    "url": "/static/js/main.eaa8aaf6.chunk.js"
+    "revision": "44687b0d256ffa86fc19",
+    "url": "/static/js/main.f487fa65.chunk.js"
   },
   {
     "revision": "8cff6654acbc05d7b9a5",
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7623314eaee7ef76d91fc0fbabd4d0c5.js"
+  "/precache-manifest.d18f9ba31787fe508c1f113d91ba8e7c.js"
 );
 
 self.addEventListener('message', (event) => {
